c3d393739ea192795968bbb1ae403b3841ba947d
Simon Garcia <gasimon12@gmail.com>
2021-05-15 21:45:40 -0400

    Creacion de archivos y v1

---
+++ kata1d2.exs
@@ -0,0 +1,7 @@
+defmodule Color do
+    def convert(red, green, blue) do
+    hex1 = Integer.to_string(red, 16)
+    hex2 = Integer.to_string(green, 16)
+    hex3 = Integer.to_string(blue, 16)
+    end
+end
