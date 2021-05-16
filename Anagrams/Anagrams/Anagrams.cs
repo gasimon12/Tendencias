1521347e4e775b8acb93508ad97f2a6a4d851548
Simon Garcia <gasimon12@gmail.com>
2021-05-16 17:07:29 -0400

    Creacion primera pruebas y funcion para determinar si son anagramas

---
+++ Anagrams/Anagrams/Anagrams.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Anagrams
+{
+    public class AnagramsClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool Check(string word1, string word2)
+        {
+            if (word1 == null || word2 == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (word1.Length != word2.Length)
+            {
+                return false;
+            }
+            else
+            {
+                word1 = Sanitize(word1);
+                word2 = Sanitize(word2);
+                if (word1 == word2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        public string Sanitize(string input)
+        {
+            char[] temp = input.ToLowerInvariant().ToCharArray();
+            Array.Sort(temp);
+            return new string(temp);
+        }
+    }
+}
--- Anagrams/AnagramsTest/UnitTest1.cs
+++ Anagrams/AnagramsTest/UnitTest1.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Anagrams;
 
 namespace AnagramsTest
 {
@@ -6,8 +8,28 @@ namespace AnagramsTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void First_Argument_Null()
         {
+            AnagramsClass anagrams = new AnagramsClass();
+            NUnit.Framework.Assert.That(() => anagrams.Check(null, "roma"), Throws.ArgumentNullException);
+        }
+        [TestMethod]
+        public void Second_Argument_Null()
+        {
+            AnagramsClass anagrams = new AnagramsClass();
+            NUnit.Framework.Assert.That(() => anagrams.Check("roma", null), Throws.ArgumentNullException);
+        }
+        [TestMethod]
+        public void Both_Arguments_Null()
+        {
+            AnagramsClass anagrams = new AnagramsClass();
+            NUnit.Framework.Assert.That(() => anagrams.Check(null, null), Throws.ArgumentNullException);
+        }
+        [TestMethod]
+        public void Arguments_Different_Lengths()
+        {
+            AnagramsClass anagrams = new AnagramsClass();
+            NUnit.Framework.Assert.AreEqual(anagrams.Check("rosita", "rosa"), false);
         }
     }
 }
