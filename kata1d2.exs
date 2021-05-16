9d110997f6056271cdb9d48805f5280e333644e5
Simon Garcia <gasimon12@gmail.com>
2021-05-15 21:48:06 -0400

    v2

--- kata1d2.exs
+++ kata1d2.exs
@@ -3,5 +3,12 @@ defmodule Color do
     hex1 = Integer.to_string(red, 16)
     hex2 = Integer.to_string(green, 16)
     hex3 = Integer.to_string(blue, 16)
+    IO.puts(hex1 <> hex2 <> hex3)
     end
 end
+
+[red, green, blue] = System.argv
+red = String.to_integer(red)
+green = String.to_integer(green)
+blue = String.to_integer(blue)
+Color.convert(red, green, blue)
