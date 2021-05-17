27b6c82d1114e8eee9768ae6fe34e4e482561b2d
Simon Garcia <gasimon12@gmail.com>
2021-05-16 21:20:54 -0400

    Finalizacion del codigo

--- kata1d3.exs
+++ kata1d3.exs
@@ -1,3 +1,11 @@
+defmodule Color do
+    def convert(input) do
+    for n <- 0..2, do:
+    Enum.at(input, n)
+    |> Integer.to_string(16)
+    |> IO.write
+    end
+end
 
 input = System.argv
 |> Enum.map(&String.to_integer\1)
