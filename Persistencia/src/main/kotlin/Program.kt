669bebfded7ecb26710028a2cdb5d12afbe6244e
Simon Garcia <gasimon12@gmail.com>
2021-06-04 19:41:28 -0400

    Avances en el algoritmo principal

--- Persistencia/src/main/kotlin/Program.kt
+++ Persistencia/src/main/kotlin/Program.kt
@@ -1,4 +1,18 @@
+import kotlin.math.*
 fun main(){
+    var temp: Long
+    var radix = 0
     print("Inserte un numero: ")
     var num = readLine()!!.toLong()
+    var digitCount = log10(num.toDouble()).plus(1).toInt()
+    do{
+        temp = 1
+        for (i in 0 until digitCount){
+            temp *= num % 10
+            num /= 10
+        }
+        num = temp
+        radix++
+        digitCount = log10(num.toDouble()).plus(1).toInt()
+    }while(digitCount > 1)
 }
