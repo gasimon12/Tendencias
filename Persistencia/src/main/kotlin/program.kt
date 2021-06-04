c15cb229a67d6dfc546b87f35a1199fc78a4f575
Simon Garcia <gasimon12@gmail.com>
2021-06-04 19:26:16 -0400

    Creacion proyecto persistencia en kotlin

---
+++ Persistencia/src/main/kotlin/program.kt
@@ -0,0 +1,20 @@
+import kotlin.math.*
+
+fun main(){
+    var temp: Long
+    var radix = 0
+    println("Inserte un numero: ")
+    var num = readLine()!!.toLong()
+    var digitCount = log10(num.toDouble()).plus(1).toInt()
+    do{
+        temp = 1
+        for(i in 0 until digitCount){
+            temp *= num % 10
+            num /= 10
+        }
+        digitCount =
+        num = temp
+        radix++
+
+    }while(digitCount > 1)
+}
