d823c92696dc33855b5402408a058dff0443ee53
Simon Garcia <gasimon12@gmail.com>
2021-06-06 21:33:12 -0400

    Avance algoritmo principal

--- Persistencia3/src/main/kotlin/main.kt
+++ Persistencia3/src/main/kotlin/main.kt
@@ -1,3 +1,5 @@
+import kotlin.math.*
+
 fun main() {
     print("Inserte un numero: ")
     try{
@@ -9,5 +11,21 @@ fun main() {
 }
 
 class Persistence{
-
+    fun calculate(num: Long, radix: Int): Int {
+        var digitCount = log10(num.toDouble()).plus(1).toInt()
+        var internal = num
+        var index = radix
+        var temp = 1L
+        for (i in 0 until digitCount){
+            temp *= internal % 10
+            internal /= 10
+        }
+        digitCount = log10(temp.toDouble()).plus(1).toInt()
+        index++
+        if (digitCount > 1){
+            return calculate(temp, index)
+        }else{
+            return index
+        }
+    }
 }
